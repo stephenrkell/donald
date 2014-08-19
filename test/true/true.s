d8473dbcf06ae93b20a00557cf5f4c2a13a6a4c1
Stephen Kell <srk31@cl.cam.ac.uk>
2014-08-19 14:19:15 +0100

    Replace out-of-repo true.s symlink with the actual code.

--- test/true/true.s
+++ test/true/true.s
@@ -1,1 +1,7 @@
-../../../Experiments/syscall-interposition/test-programs/true.s
+	.section	.text
+	.globl _start
+_start:
+	movq $60, %rax		# exit
+    movq _r_debug, %rdi # create a dependency on the dynamic linker
+    movq $0x0, %rdi
+	syscall
