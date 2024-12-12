5eb4feffdeaf31a0015d8e9fb84ab88897a1bf59
Stephen Kell <srk31@srcf.ucam.org>
2024-12-12 12:33:58 +1100

    debug.c: provide fake_ld_so_link_map for our extending clients' use

--- src/debug.c
+++ src/debug.c
@@ -50,6 +50,10 @@ ElfW(Dyn) *find_or_create_dt_debug(uintptr_t inferior_load_addr, uintptr_t infer
 	return d;
 }
 
+// other client code may want this
+__attribute__((visibility("hidden")))
+struct link_map fake_ld_so_link_map;
+
 __attribute__((visibility("hidden")))
 void populate_dt_debug(ElfW(Dyn) *d, uintptr_t inferior_load_addr,
 	uintptr_t inferior_dynamic_vaddr, uintptr_t inferior_r_debug_vaddr)
@@ -57,7 +61,7 @@ void populate_dt_debug(ElfW(Dyn) *d, uintptr_t inferior_load_addr,
 	struct r_debug *r = (struct r_debug *)(inferior_load_addr + inferior_r_debug_vaddr);
 	/* What about the contents of the inferior's r_debug? To enable
 	 * debugging early in the ld.so, we could create a fake entry. Let's try it. */
-	static struct link_map fake_link_map = {
+	fake_ld_so_link_map = (struct link_map) {
 		.l_addr = 0 /* inferior_load_addr */,
 		.l_name = SYSTEM_LDSO_PATH,
 		.l_ld = 0 /* (ElfW(Dyn) *) inferior_dynamic_vaddr */,
@@ -65,13 +69,13 @@ void populate_dt_debug(ElfW(Dyn) *d, uintptr_t inferior_load_addr,
 		.l_next = NULL
 	};
 	extern void _dl_debug_state(void);
-	fake_link_map.l_addr = inferior_load_addr;
-	fake_link_map.l_ld = (ElfW(Dyn) *) (inferior_load_addr + inferior_dynamic_vaddr);
+	fake_ld_so_link_map.l_addr = inferior_load_addr;
+	fake_ld_so_link_map.l_ld = (ElfW(Dyn) *) (inferior_load_addr + inferior_dynamic_vaddr);
 	// FIXME: this causes glibc's ld.so to jump to (void*)-1... understand why
 #if 0
 	*r = (struct r_debug) {
 		.r_version = 1,
-		.r_map = &fake_link_map,
+		.r_map = &fake_ld_so_link_map,
 		.r_brk = (uintptr_t) _dl_debug_state,
 		.r_state = RT_CONSISTENT,
 		.r_ldbase = inferior_load_addr
